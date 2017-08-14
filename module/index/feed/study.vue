035e4a6e7c65bc258fc580ea78b19a21bf338710
mahenan <676962537@qq.com>
2017-08-14 17:48:26 +0800

    mahenan

--- module/index/feed/study.vue
+++ module/index/feed/study.vue
@@ -1,25 +1,40 @@
 <template>
-  <div>
+  <div class="con">
     <div class="container">
       <div class="title">
         <img src="//pic.davdian.com/free/2017/08/14/free_study.png" alt="">
       </div>
     </div>
-    <div class="list_all">
-      <div class="list">
-        <div class="list_left">
-          <div class="list_content">第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打</div>
-          <div class="times">
-            <div class="time">04:19</div>
-            <div class="name">《搞笑西游记》</div>
+    <div class="all">
+      <div class="list_all">
+        <div class="list">
+          <div class="list_left">
+            <div class="list_content">第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打</div>
+            <div class="times">
+              <div class="time">04:19</div>
+              <div class="name">《搞笑西游记》</div>
+            </div>
+          </div>
+          <div class="list_right">
+            <img src="//pic.davdian.com/free/2017/08/14/5422b1ff86ed0232.jpg!200x200.jpg" alt="">
           </div>
         </div>
-        <div class="list_right">
-          <img src="//pic.davdian.com/free/2017/08/14/5422b1ff86ed0232.jpg!200x200.jpg" alt="">
+      </div>
+      <div class="list_all">
+        <div class="list">
+          <div class="list_left">
+            <div class="list_content">第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打第78回: 三打白骨精没打死继续打打打打</div>
+            <div class="times">
+              <div class="time">04:19</div>
+              <div class="name">《搞笑西游记》</div>
+            </div>
+          </div>
+          <div class="list_right">
+            <img src="//pic.davdian.com/free/2017/08/14/5422b1ff86ed0232.jpg!200x200.jpg" alt="">
+          </div>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 <script>
@@ -28,6 +43,9 @@
   }
 </script>
 <style scoped>
+  .con{
+    background: white;
+  }
   .container{
     margin-top: 0.1rem;
     text-align:right;
@@ -35,8 +53,8 @@
     margin-right: 0.1rem;
   }
   .title{
-    margin-top: 0.15rem;
-    margin-bottom: 0.05rem;
+    padding-top: 0.15rem;
+    padding-bottom: 0.05rem;
     height: 0.22rem;
   }
   .title img{
@@ -45,7 +63,7 @@
     height: 0.16rem;
   }
   .list{
-    padding-top:0.08rem;
+    padding-top:0.1rem;
     padding-bottom:0.1rem;
     font-size: 0;
     position: relative;
@@ -66,9 +84,7 @@
     max-width: 3rem;
     overflow: hidden;
     text-overflow: ellipsis;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    display: -webkit-box;
+    white-space: nowrap;
     margin-bottom: 0.05rem;
   }
   .list_right img{
@@ -80,9 +96,23 @@
     padding: 0 0.1rem;
     border-top: 1px solid #E1E1E1;
   }
-  .time{
-    padding-right: 10px;
-    border: 1px solid #999999;
+  .times>div{
+    display: inline-block;
+    font-size: 0;
+    line-height:0.16rem;
+  }
+  .times .time{
+    padding-right: 0.1rem;
+    border-right: 1px solid #999999;
     font-size:11px;
+    margin-right:0.05rem;
+  }
+  .times .name{
+    font-size: 11px;
+  }
+  .all .list_all:nth-child(1){
+    border: 0;
   }
 </style>
+
+
