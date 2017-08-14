5e64749e0c32a02cd6890329ff300ef6fca37ed1
murphysweb@163.com <murphy1991116>
2017-08-14 10:55:12 +0800

    jinlong

---
+++ src/page/login/js/login.js
@@ -0,0 +1,69 @@
+// 基础模块
+import common from '../../../common/js/common.js';
+
+// 第三方模块
+import Vue from 'Vue';
+import $ from '$';
+import Cookies from 'js-cookie';
+
+// 业务模块
+import encrypt from '../../../common/js/module/encrypt.js';
+import util from '../../../common/js/module/util.js';
+import tj from '../../../common/js/module/tj.js';
+import popup from '../../../common/js/module/popup.js';
+import login from '../../../common/js/module/login.js';
+import native from '../../../common/js/module/native.js';
+// 渲染页面
+new Vue({
+  el: ".app",
+  components: {
+    'com-top-title': require('../../../component/com-top-title.vue'),
+    'com-to-top-icon': require('../../../component/com-to-top-icon.vue'),
+    'login-container': require('../vue/login-container.vue'),
+  },
+  data() {
+    return {
+      title:"登录",
+      btn:{
+        "name":"会员介绍",
+        "href":"www.baidu.com"
+      }
+    }
+  },
+  computed: {},
+  watch: {
+
+  },
+  beforeCreate() {
+
+  },
+  created() {
+    // this.getData();
+  },
+  methods: {
+    /**
+     * 接口名称:
+     * 接口文档:
+     */
+    getData(){
+      let ts = this;
+      $.ajax({
+        cache: false,
+        async: true,
+        url: '?_=' + Date.now(),
+        type: 'post',
+        dataType: 'json',
+        data: encrypt({
+          js_wx_info: 1,
+        }),
+        success(response) {
+          ts.response = response;
+        },
+        error(error) {
+          ts.response = require('../json/login.json');
+          console.error('ajax error:' + error.status + ' ' + error.statusText);
+        }
+      });
+    },
+  }
+});
---
+++ src/page/login/vue/login-container.vue
@@ -0,0 +1,131 @@
+<template>
+  <!--登录表单-->
+  <!--正常登陆-->
+  <div>
+    <div class="inputbox" style="margin-top: 50px">
+      <input type="tel" placeholder="请输入您的手机号" v-model="mobile" name="mobile">
+      <img src="../img/clearInput.png" v-if="mobile != ''" v-on:click="mobile = ''">
+    </div>
+    <div class="inputbox">
+      <input type="tel" placeholder="请输入密码" v-model="password" name="password">
+      <img src="../img/clearInput.png" v-if="password != ''" v-on:click="password = ''">
+    </div>
+    <!--登录或注册按钮-->
+    <div v-if="mobile == '' || password == ''" class="loginbtn" style="opacity: 0.4">登录</div>
+    <div v-else class="loginbtn">登录</div>
+    <!--忘记密码和注册账号按钮-->
+    <div class="forget_sign">
+      <a class="forgets">忘记密码</a>
+      <a class="sign">注册会员</a>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: {},
+    data() {
+      return {
+        mobile: '',
+        password:''
+      }
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+
+    },
+    methods: {}
+  }
+</script>
+<style lang="sass" lang="scss" rel="stylesheet/scss" scoped>
+  .inputbox {
+    width: 78.666%;
+    height: 40px;
+    background-color: #F8F8F8;
+    margin: 15px auto;
+    border-radius: 20px;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .inputbox input {
+    background-color: #F8F8F8F8;
+    font-size: 14px;
+    line-height: 20px;
+    color: #333333;
+    padding: 10px 30px 10px 16px;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 1px;
+    border: none;
+  }
+
+  .inputbox img {
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    top: 12px;
+    right: 12px;
+  }
+
+  *:focus {
+    outline: none;
+  }
+
+  ::-webkit-input-placeholder {
+    color: #999;
+  }
+
+  :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+    color: #999;
+  }
+
+  ::-moz-placeholder { /* Mozilla Firefox 19+ */
+    color: #999;
+  }
+
+  :-ms-input-placeholder { /* Internet Explorer 10+ */
+      color: #999;
+  }
+  .loginbtn{
+    width: 73.3333%;
+    height: 40px;
+    margin: 25px auto;
+    text-align: center;
+    line-height: 40px;
+    font-size: 14px;
+    color: #FFFFFF;
+    background: -webkit-linear-gradient(left, #FF5C5C, #FA1862);
+    background: linear-gradient(to right, #FF5C5C, #FA1862);
+    border-radius: 20px;
+    font-weight: 300;
+  }
+  .disablebtn{
+    opacity: 0.4;
+  }
+  .forget_sign{
+    width: 78.666%;
+    height: 17px;
+    line-height: 17px;
+    padding-top: 17px;
+    margin: 0 auto;
+    font-size:12px;
+  }
+  .forget_sign a{
+    display: inline-block;
+    font-size:12px;
+    color:#999999;
+  }
+  .forgets{
+    float: left;
+  }
+  .sign{
+    float: right;
+  }
+</style>
+<style>
+  body, html {
+    background-color: #FFFFFF;
+  }
+</style>
