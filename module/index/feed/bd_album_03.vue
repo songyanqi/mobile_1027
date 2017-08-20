ae928812954ccc6086624bfe170aa8bff620a184
mahenan <676962537@qq.com>
2017-08-20 17:58:10 +0800

    mahenan

--- module/index/feed/bd_album_03.vue
+++ module/index/feed/bd_album_03.vue
@@ -36,8 +36,8 @@
             </div>
             <div class="item_right" v-if="item.isFree==1">
               <div class="disable" v-if="item.isPlay==0 && isSub==0" @click.stop="stop_info"><img src="//pic.davdian.com/free/2017/08/16/Group1.png" alt=""></div>
-              <div class="mask_stop" v-if="isSub==1 && item.isPlay==1 && !(item.albumId==albumId && item.sortNo==sortNo && btnStatus==1)" @click.stop="go_play(item.albumId,item.sortNo)"><img src="//pic.davdian.com/free/2017/08/16/b_stop.png" alt=""></div>
-              <div class="mask_play" v-if="isSub==1 && item.isPlay==1 && (item.albumId==albumId && item.sortNo==sortNo && btnStatus==1)" @click.stop="go_play(item.albumId,item.sortNo)"><img src="//pic.davdian.com/free/2017/08/16/b_play.png" alt=""></div>
+              <div class="mask_stop" v-if="isSub==1 && item.isPlay==1 && (item.albumId==albumId && item.sortNo==sortNo && btnStatus==1)" @click.stop="go_play(item.albumId,item.sortNo)"><img src="//pic.davdian.com/free/2017/08/16/b_stop.png" alt=""></div>
+              <div class="mask_play" v-if="isSub==1 && item.isPlay==1 && !(item.albumId==albumId && item.sortNo==sortNo && btnStatus==1)" @click.stop="go_play(item.albumId,item.sortNo)"><img src="//pic.davdian.com/free/2017/08/16/b_play.png" alt=""></div>
               <div class="circle_mask"></div>
               <div><img :src="item.imageUrl" alt=""></div>
             </div>
@@ -84,6 +84,10 @@
             "result":'audioPlayHistoryFn'
           })
         });
+
+        this.$nextTick(function () {
+          this.audioLocation();
+        });
       },
       data(){
           return {
@@ -102,6 +106,16 @@
           }
       },
       methods:{
+          audioLocation(){
+            if(this.isApp){
+              window.audioLocationFn=function (obj) {
+                window.iosInterface.getAudioState(obj);
+              };
+              native.Audio.audioLocation({
+                "result":audioLocationFn
+              })
+            }
+          },
           tab_scroll(){
 
               var tab=$(".top").offset().top;
