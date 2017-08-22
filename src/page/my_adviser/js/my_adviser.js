c97c86663505d02cff89b1b6d08f7171e75c552c
murphysweb@163.com <murphy1991116>
2017-08-22 10:06:41 +0800

    金龙

---
+++ src/page/my_adviser/js/my_adviser.js
@@ -0,0 +1,75 @@
+// 基础模块
+import common from '../../../common/js/common.js';
+
+// 第三方模块
+import Vue from 'Vue';
+import $ from '$';
+
+// 业务模块
+import encrypt from '../../../common/js/module/encrypt.js';
+import popup from '../../../common/js/module/popup.js';
+import login from '../../../common/js/module/login.js';
+import native from '../../../common/js/module/native.js';
+
+login.needLogin();
+
+// 渲染页面
+new Vue({
+  el: ".app",
+  components: {
+    'com-top-title': require('../../../component/com-top-title.vue')
+  },
+  data() {
+    return {
+      response: null,
+    }
+  },
+  computed: {},
+  watch: {
+    // 监听response变化
+    response(){
+      // response变化后并渲染完dom,设置其他事项
+      this.$nextTick(function () {
+        let ts = this;
+
+        // 设置app头部标题栏
+        native.custom.initHead({
+          shareOnHead: 1,
+        });
+
+      });
+    }
+  },
+  beforeCreate() {
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    /**
+     * 接口名称:
+     * 接口文档:
+     */
+    getData(){
+      let ts = this;
+      $.ajax({
+        cache: false,
+        async: true,
+        url: '?_=' + Date.now(),
+        type: 'post',
+        dataType: 'json',
+        data: encrypt({
+          js_wx_info: 1,
+        }),
+        success(response) {
+          ts.response = response;
+        },
+        error(error) {
+          ts.response = require('../json/my_adviser.json');
+          console.error('ajax error:' + error.status + ' ' + error.statusText);
+        }
+      });
+    },
+  },
+  filters: {},
+});
