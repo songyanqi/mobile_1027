2d0a11cb6a1346e0946dc0b8f9ec1b6c37d49722
jianchep <971386910@qq.com>
2017-08-28 16:55:34 +0800

    commit

--- src/page/collect/vue/collect.vue
+++ src/page/collect/vue/collect.vue
@@ -127,12 +127,13 @@
                       'isShowAudio':1,
                     });
                   }
-                  // native.Browser.setHead({
-                  //   'title':that.title,
-                  //   'backBtn':'1',
-                  //   'shareBtn':"1",
-                  //   'shareMoney':result.data.attr.income,
-                  // })
+                  
+                  native.Browser.setHead({
+                    'title':that.title,
+                    'backBtn':'1',
+                    'shareBtn':"1",
+                    'shareMoney':result.data.attr.income,
+                  })
 
                 }else{
                   that.maskFlag=true;
