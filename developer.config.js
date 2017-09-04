f76980507e8daebb0f43c88c3632e326d7489fe0
mahenan <676962537@qq.com>
2017-09-04 21:27:24 +0800

    mahenan

--- developer.config.js
+++ developer.config.js
@@ -15,6 +15,6 @@
 export default {
   //env_stage: 'beta',
   //env_num: '2',
-  page: '{my_adviser,choose_mama_adviser,my_invitation_code}',
-  webpack: false,
+  //page: '{my_adviser,choose_mama_adviser,my_invitation_code}',
+  //webpack: false,
 };
