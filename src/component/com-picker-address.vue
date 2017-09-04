c6123901617c9e8032ac11bbeb8799553064df05
murphysweb@163.com <murphy1991116>
2017-09-04 13:36:25 +0800

    picker问题修复

--- src/component/com-picker-address.vue
+++ src/component/com-picker-address.vue
@@ -6,10 +6,8 @@
     </div>
   </div>
 </template>
-
 <script>
-  import {Picker} from 'vux';
-//  import axios from 'axios';
+  import Picker from "../page/vux-fix/picker.vue";
   export default {
     components: {
       Picker
@@ -137,7 +135,65 @@
         addressNow:[],
         addressValue: [],
         addressPre:[],
-        addressNameList:[]
+        addressNameList:[],
+        year7: [{
+          name: '中国',
+          value: 'china',
+          parent: 0
+        }, {
+          name: '美国',
+          value: 'USA',
+          parent: 0
+        }, {
+          name: '广东',
+          value: 'china001',
+          parent: 'china'
+        }, {
+          name: '广西',
+          value: 'china002',
+          parent: 'china'
+        }, {
+          name: '美国001',
+          value: 'usa001',
+          parent: 'USA'
+        }, {
+          name: '美国002',
+          value: 'usa002',
+          parent: 'USA'
+        }, {
+          name: '广州',
+          value: 'gz',
+          parent: 'china001'
+        }, {
+          name: '深圳',
+          value: 'sz',
+          parent: 'china001'
+        }, {
+          name: '广西001',
+          value: 'gz',
+          parent: 'china002'
+        }, {
+          name: '广西002',
+          value: 'sz',
+          parent: 'china002'
+        }, {
+          name: '美国001_001',
+          value: '0003',
+          parent: 'usa001'
+        }, {
+          name: '美国001_002',
+          value: '0004',
+          parent: 'usa001'
+        }, {
+          name: '美国002_001',
+          value: '0005',
+          parent: 'usa002'
+        }, {
+          name: '美国002_002',
+          value: '0006',
+          parent: 'usa002'
+        }],
+        year7Value: []
       }
     }
   }
@@ -189,5 +245,4 @@
       transform: translateY(0%);
     }
   }
-
 </style>
