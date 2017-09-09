d661d1993a4fa3409eba7160e0f7b17958c31120
donyyang <592645778@qq.com>
2017-09-09 11:04:33 +0800

    modfiy: 提交订单详情页

--- javascript/cartConfirm.js
+++ javascript/cartConfirm.js
@@ -664,7 +664,8 @@ $(function(){
     }
 
     function pay() {
-        if (!$(".order_rule_ipt").hasClass("order_rule_ipt_checked")) {
+      // 如果不是预定商品就不走这一行代码
+        if ($(".order_rule_ipt") && !$(".order_rule_ipt").hasClass("order_rule_ipt_checked")) {
           $(".order_navList").addClass("order_navList_line");
           bravetime.info("请先同意预售规则，才能去支付哦～")
           return;
@@ -1021,12 +1022,15 @@ $(function(){
         }
     }
     // 预定商品点击相关规则
-    $(".order_navList").on("click",function () {
+    $(".order_navList").on("click",function (e) {
+      if (e.target.className == "dealIcon") {
+        console.log("dealIcon");
+        return;
+      }
       $(this).find(".order_rule_ipt").toggleClass("order_rule_ipt_checked");
       if ($(".order_rule_ipt").hasClass("order_rule_ipt_checked")) {
         $(this).removeClass("order_navList_line");
       }
     });
-
 });
 
