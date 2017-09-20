a761b7d36e63c0712cfc0a1a4f890788f787c2ce
donyyang <592645778@qq.com>
2017-09-20 14:42:28 +0800

    联调

--- javascript/cartConfirm.js
+++ javascript/cartConfirm.js
@@ -665,13 +665,13 @@ $(function(){
 
     function pay() {
       //如果不是预定商品就不走这一行代码
-        // if (is_advance && is_advance == "1") {
-        //   if (!$(".order_rule_ipt").hasClass("order_rule_ipt_checked")) {
-        //     $(".order_navList").addClass("order_navList_line");
-        //     bravetime.info("请先同意预售规则，才能去支付哦～")
-        //     return;
-        //   }
-        // }
+        if (is_advance && is_advance == "1") {
+          if (!$(".order_rule_ipt").hasClass("order_rule_ipt_checked")) {
+            $(".order_navList").addClass("order_navList_line");
+            bravetime.info("请先同意预售规则，才能去支付哦～")
+            return;
+          }
+        }
         var payData = {}, el;
 
         // 校验身份证
@@ -1025,7 +1025,7 @@ $(function(){
     // 预定商品点击相关规则
     $(".order_navList").on("click",function (e) {
       if (e.target.className == "dealIcon") {
-        console.log("dealIcon");
+        location.href = "/t-14491.html";
         return;
       }
       $(this).find(".order_rule_ipt").toggleClass("order_rule_ipt_checked");
