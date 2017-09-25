c4d002999b0108f4238b52932ab16550a91968de
mahenan <676962537@qq.com>
2017-09-25 11:39:58 +0800

    click

--- developer.config.js
+++ developer.config.js
@@ -15,6 +15,6 @@
 export default {
   //env_stage: 'beta',
   //env_num: '2',
-  //page: '{act_1018_all_together_assistance,act_1018_main_subscribe,act_1018_main,lightBrand,act_1018_assistance}',
-  //webpack: false,
+  page: '{act_1018_all_together_assistance,act_1018_main_subscribe,act_1018_main,lightBrand,act_1018_assistance}',
+  webpack: false,
 };
--- src/page/act_1018_main/vue/lightBrand.vue
+++ src/page/act_1018_main/vue/lightBrand.vue
@@ -13,11 +13,10 @@
       </div>
     </div>
     <div class="all_list">
-      <div class="list" v-for="(item,index) in response"  :class="{'light':item.isCompleted==1}">
-        <div class="list_b_img" @click.stop="go_detail(item.linkUrl)">
+      <div class="list" @click="go_detail(item.linkUrl)" v-for="(item,index) in response"  :class="{'light':item.isCompleted==1}">
+        <div class="list_b_img">
           <img v-lazy="item.bandPic">
         </div>
-
         <div class="list_bottom" v-if="item.hotDay==-1">
 
           <template v-if="item.isCompleted!=1">
