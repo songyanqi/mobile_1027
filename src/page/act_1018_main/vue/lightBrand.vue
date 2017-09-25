26c5c69cef3782051c837a41e6a84b2b240568bb
mahenan <676962537@qq.com>
2017-09-25 17:01:08 +0800

    mahenan

--- src/page/act_1018_main/vue/lightBrand.vue
+++ src/page/act_1018_main/vue/lightBrand.vue
@@ -22,7 +22,7 @@
 
         <div class="list_bottom" v-if="item.hotDay==-1">
 
-          <template v-if="item.isCompleted!=1">
+          <template v-if="isCompleted[index]!=1">
             <div class="list_start_border"></div>
             <div class="list_start">
               <img class="start" v-lazy="'//pic.davdian.com/free/2017/09/09/start_icon.png'">
@@ -31,7 +31,7 @@
             <div class="line"></div>
           </template>
 
-          <template v-if="item.isCompleted==1">
+          <template v-if="isCompleted[index]==1">
             <div class="success">
               <div style="position: absolute;">
                 <img v-lazy="'//pic.davdian.com/free/2017/09/09/com.png'">
