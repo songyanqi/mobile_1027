2763b1220fec20e5714655d37d5573a9d64b5f3c
sunweiguang <sunweiguang@davdian.com>
2017-09-29 17:12:57 +0800

    1018第二波 中奖纪录

--- src/page/act_1018_mine/js/act_1018_mine.js
+++ src/page/act_1018_mine/js/act_1018_mine.js
@@ -136,7 +136,9 @@ new Vue({
         }),
         success(response) {
           common.checkRedirect(response);
-          ts.response = response;
+
+          // ts.response = response;
+          ts.response = require('../json/act_1018_mine.json');
 
           // 存缓存
           // localCache.setItem({
