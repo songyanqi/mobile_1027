fdd810614b06c420aad881e24371b06793fd4d56
sunweiguang <sunweiguang@davdian.com>
2017-10-12 16:13:10 +0800

    1018第二波

--- src/component/com-top-title.vue
+++ src/component/com-top-title.vue
@@ -83,8 +83,12 @@
           if (history.length > 1) {
             history.back();
           } else {
-            window.location.href = "about:blank";
-            window.close();
+            if (ua.isWeiXin() && window.wx) {
+              window.wx.closeWindow();
+            } else {
+              window.location.href = "about:blank";
+              window.close();
+            }
           }
         }
       },
