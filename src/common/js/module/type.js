23d823e194024bb367ac67ceae9e22b6048d4b18
sunweiguang <sunweiguang@davdian.com>
2017-10-13 15:34:42 +0800

    1018第二波

---
+++ src/common/js/module/type.js
@@ -0,0 +1,17 @@
+export default {
+  isNumber(value){
+    return Object.prototype.toString.call(value) === '[object Number]';
+  },
+  isString(value){
+    return Object.prototype.toString.call(value) === '[object String]';
+  },
+  isBoolean(value){
+    return Object.prototype.toString.call(value) === '[object Boolean]';
+  },
+  isObject(value){
+    return Object.prototype.toString.call(value) === '[object Object]';
+  },
+  isArray(value){
+    return Object.prototype.toString.call(value) === '[object Array]';
+  },
+}
