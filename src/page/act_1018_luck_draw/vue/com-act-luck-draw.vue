be10ce70b5c0f9785d1d97d3be76d13bb6241e90
donyyang <592645778@qq.com>
2017-10-15 00:18:36 +0800

    add bouns

--- src/page/act_1018_luck_draw/vue/com-act-luck-draw.vue
+++ src/page/act_1018_luck_draw/vue/com-act-luck-draw.vue
@@ -8,82 +8,132 @@
     <div class = "luckCont" id="lottery">
       <table border="0" cellpadding="0" cellspacing="0">
     <tr>
-      <td class="lottery-unit lottery-unit-0"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/1018money.png"></td>
-      <td class="lottery-unit lottery-unit-1"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/1018money1.png"></td>
-      <td class="lottery-unit lottery-unit-2"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/luckbtn.png"></td>
-      <td class="lottery-unit lottery-unit-3"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/meme_icon.png"></td>
+      <td class="lottery-unit lottery-unit-0"><img src="http://pic.davdian.com/avatar_poster/1018money.png"></td>
+      <td class="lottery-unit lottery-unit-1"><img src="http://pic.davdian.com/avatar_poster/renxing_icon.png"></td>
+      <td class="lottery-unit lottery-unit-2"><img src="http://pic.davdian.com/avatar_poster/1018money1.png"></td>
     </tr>
     <tr>
-      <td class="lottery-unit lottery-unit-11"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/mysterious_icon.png"></td>
-      <td colspan="2" rowspan="2"><a href=""></a></td>
-      <td class="lottery-unit lottery-unit-4"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/renxing_icon.png"></td>
+      <td class="lottery-unit lottery-unit-7"><img src="http://pic.davdian.com/avatar_poster/mysterious_icon.png"></td>
+      <td @click = "handleClick"><img src="http://pic.davdian.com/avatar_poster/luckbtn.png"></td>
+      <td class="lottery-unit lottery-unit-3"><img src="http://pic.davdian.com/avatar_poster/meme_icon.png"></td>
     </tr>
     <tr>
-      <td class="lottery-unit lottery-unit-10"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/suprice_icon.png"></td>
-      <td class="lottery-unit lottery-unit-5"><img src="http://mamaj-oss.oss-cn-beijing.aliyuncs.com/avatar_poster/thanks_icon.png"></td>
-    </tr>
-        <tr>
-      <td class="lottery-unit lottery-unit-9"><img src="http://mamaj-oss-ws.oss-cn-beijing.aliyuncs.com/free/Zhuanti/modify_new_avatar.png"></td>
-      <td class="lottery-unit lottery-unit-8"><img src="http://mamaj-oss-ws.oss-cn-beijing.aliyuncs.com/free/Zhuanti/modify_new_avatar.png"></td>
-      <td class="lottery-unit lottery-unit-7"><img src="http://mamaj-oss-ws.oss-cn-beijing.aliyuncs.com/free/Zhuanti/modify_new_avatar.png"></td>
-            <td class="lottery-unit lottery-unit-6"><img src="http://mamaj-oss-ws.oss-cn-beijing.aliyuncs.com/free/Zhuanti/modify_new_avatar.png"></td>
+      <td class="lottery-unit lottery-unit-6"><img src="http://pic.davdian.com/avatar_poster/thanks_icon.png"></td>
+      <td class="lottery-unit lottery-unit-5"><img src="http://pic.davdian.com/avatar_poster/suprice_icon.png"></td>
+      <td class="lottery-unit lottery-unit-4"><img src="http://pic.davdian.com/avatar_poster/god_icon.png"></td>
     </tr>
   </table>
     </div>
 	</div>
 </template>
 <script type="text/javascript">
+  import login from '../../../common/js/module/login.js';
   import encrypt from '../../../common/js/module/encrypt.js';
   import popup from '../../../common/js/module/popup.js';
   import native from '../../../common/js/module/native.js';
 
   export default {
-  	components: {
+    components: {
 
-  	},
-  	data () {
-  		return {
-  			luckNum: 0,
+    },
+    data () {
+      return {
+        luckNum: 0,
+        // 是否有抽奖机会
+        isluck: false,
 
         index:-1,
         count:0,
         timer:0,
         speed:20,
         times:0,
-        cycle:50,
+        cycle:25,
         prize:-1,
-  		}
-  	},
-    props: [],
-  	created() {
-  		// this.getData();
-      this.init();
-  	},
-    mounted() {
-
+        click: false,
+        isLogin: false,
+        visitorStatus: 1,
+      }
     },
-  	watch: {
-      
-  	},
-  	methods: {
+    props: [],
+    created() {
+      this.isLogin = login.isLogined();
+      this.getData();
+    },
+    watch: {
+      luckNum: {
+        handler() {
+          this.$nextTick(function() {
+            this.init("lottery");
+          })
+        },
+        deep: true,
+      },
+    },
+    methods: {
       getData() {
-        lottery.init('lottery');
-        $("#lottery a").click(function(){
-          if(click) {
-            return false;
-          }
-          else{
-            lottery.speed=100;
-            roll();
-            click=true;
-            return false;
-          }
-        });
+        let that = this;
+        $.ajax({
+          url: " https://www.easy-mock.com/mock/59b9230be0dc663341a8ce57/clickbouns",
+          type: "GET",
+          dataType: "JSON",
+          success(res) {
+            if (!res.code) {
+              that.luckNum = Number(res.data.number);
+              that.visitorStatus = res.visitor_status;
+              if (res.data.number != '0') {
+                that.isluck = true;
+              }
+            }
+          },
+          error(error) {
+            console.log('error',error);
+          },
+        })
+      },
+      handleClick() {
+        // 是否登陆
+        if (!this.isLogin) {
+          location.href = "/login.html?referer=" + encodeURI(location.href);
+          return;
+        }
+        if (this.visitorStatus != 3) {
+          popup.confirm({
+            title: '您还没有成为会员，不能参与抽奖哟，成为会员立即抽1018元现金大奖吧～',
+            text: '',
+            okBtnTitle: '开通会员',
+            okBtnCallback() {
+              location.href = "/index.php?c=ShopGoods&a=index&id=348&rp=index&rl=shop_button";
+            },
+            cancelBtnTitle: '取消',
+          });
+          return;
+        }
+        // 是否有抽奖机会
+        if (!this.isluck) {
+          popup.toast("订单累计实际支付金额满500元才能抽奖哦～");
+          return;
+        }
+
+        if (this.luckNum == '0') {
+          popup.toast("抽奖机会已用完，订单累计实际支付金额每满500元可抽奖一次～");
+          return;
+        }
+
+
+        if(this.click) {
+          return false;
+        }
+        else{
+          this.speed=100;
+          this.getInit();
+          this.click=true;
+          return false;
+        }
       },
       init(id) {
         if ($("#"+id).find(".lottery-unit").length>0) {
-          $lottery = $("#"+id);
-          $units = $lottery.find(".lottery-unit");
+          let $lottery = $("#"+id);
+          let $units = $lottery.find(".lottery-unit");
           this.obj = $lottery;
           this.count = $units.length;
           $lottery.find(".lottery-unit-"+this.index).addClass("active");
@@ -95,18 +145,18 @@
         var lottery = this.obj;
         $(lottery).find(".lottery-unit-"+index).removeClass("active");
         index += 1;
-        if (index>count-1) {
+        if (index>this.count-1) {
           index = 0;
         };
         $(lottery).find(".lottery-unit-"+index).addClass("active");
         this.index=index;
         return false;
       },
-      stop(index) {
-        this.prize=index;
-        return false;
-        console.log("this.price",this.prize);
-      },
+      // stop(index) {
+      //   this.prize=index;
+      //   return false;
+      //   console.log("this.price",this.prize);
+      // },
       getInit() {
         let that = this;
         that.times += 1;
@@ -115,8 +165,21 @@
             clearTimeout(that.timer);
             that.prize=-1;
             that.times=0;
-            click=false;
-            console.log(that.index);
+            that.click=false;
+            $.ajax({
+              url: " https://www.easy-mock.com/mock/59b9230be0dc663341a8ce57/bounsDes",
+              type: "GET",
+              dataType: "JSON",
+              success(res) {
+                console.log("res",res);
+              },
+              error(error) {
+                console.log('error',error);
+              },
+            })
+
+
+            console.log(1111111,that.index);
           }else{
             if (that.times<that.cycle) {
               that.speed -= 10;
@@ -133,37 +196,10 @@
             if (that.speed<40) {
               that.speed=40;
             };
-            that.timer = setTimeout(roll,that.speed);
+            that.timer = setTimeout(that.getInit,that.speed);
           }
           return false;
       },
-
-
-
-
-
-      isApp() {
-        let u = navigator.userAgent;
-
-        return !!u.match(/davdian|bravetime|vyohui/);
-      },
-      // 跳转方式
-      handleJump(url) {
-        this.isapp = this.isApp();
-        if (this.isapp) {
-          event.preventDefault();
-          native.Browser.open({
-            url: url
-          });
-        } else if (this.isMobile()) {
-          window.open(url, '_blank');
-        } else {
-          window.open(url, '_self');
-        }
-      },
-  		getData() {
-  			
-  		}
-  	},
+    }
   }
 </script>
