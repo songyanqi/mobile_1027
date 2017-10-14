8bddfcadc4db2d5d5c3d46d96e1c6717bef51e19
donyyang <592645778@qq.com>
2017-10-14 15:03:38 +0800

    add luck

---
+++ src/page/act_1018_luck_draw/vue/com-act-luck-draw.vue
@@ -0,0 +1,55 @@
+<style  lang="sass"  rel="stylesheet/scss">
+  
+</style>
+<template>
+	<div class = "luckDraw">
+    <div></div>
+	</div>
+</template>
+<script type="text/javascript">
+  import encrypt from '../../../common/js/module/encrypt.js';
+  import popup from '../../../common/js/module/popup.js';
+  import native from '../../../common/js/module/native.js';
+
+  export default {
+  	components: {
+
+  	},
+  	data () {
+  		return {
+  			
+  		}
+  	},
+    props: [],
+  	created() {
+  		// this.getData();
+  	},
+  	watch: {
+      
+  	},
+  	methods: {
+      isApp() {
+        let u = navigator.userAgent;
+
+        return !!u.match(/davdian|bravetime|vyohui/);
+      },
+      // 跳转方式
+      handleJump(url) {
+        this.isapp = this.isApp();
+        if (this.isapp) {
+          event.preventDefault();
+          native.Browser.open({
+            url: url
+          });
+        } else if (this.isMobile()) {
+          window.open(url, '_blank');
+        } else {
+          window.open(url, '_self');
+        }
+      },
+  		getData() {
+  			
+  		}
+  	},
+  }
+</script>
