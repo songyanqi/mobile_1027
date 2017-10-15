54cd16df1c0056ddc7f21c10b6dd969733ef7cc9
donyyang <592645778@qq.com>
2017-10-15 15:21:58 +0800

    add bouns

--- src/page/act_1018_bouns_rain/vue/com-act-bouns-rain.vue
+++ src/page/act_1018_bouns_rain/vue/com-act-bouns-rain.vue
@@ -4,6 +4,7 @@
 </style>
 <template>
 	<div class = "bounsCont">
+
 		<div v-if = "isConfirm">
 	    <div class = "weui-mask"></div>
 	    <div class="weui-dialog">
@@ -371,7 +372,7 @@ import popup from '../../../common/js/module/popup.js';
 	                  $(".min_text").hide();
 	                };
 	              } else {
-	                showResult();
+	                that.showResult();
 	              }
 	            } else {
 	              popup.toast(result.data.msg);
@@ -379,7 +380,7 @@ import popup from '../../../common/js/module/popup.js';
 	            }
 	          }, error: function () {
 	            popup.toast("网络异常，请稍后重试")
-	            showResult();
+	            that.showResult();
 	          }
 	        });
 	      }
