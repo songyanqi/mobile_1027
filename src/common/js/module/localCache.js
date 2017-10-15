76a57aa09591c3a9dc0914edf7efb07954b9c32a
sunweiguang <sunweiguang@davdian.com>
2017-10-15 16:23:10 +0800

    1018第二波

--- src/common/js/module/localCache.js
+++ src/common/js/module/localCache.js
@@ -7,6 +7,8 @@ const one = {
   d: 86400000,
 };
 
+const moduleName = `localCache模块`;
+
 export default {
   /**
    * 存缓存数据
@@ -26,7 +28,7 @@ export default {
   setItem(param = {}) {
     // 参数检查-缓存key(不进行缓存的条件：key为空)
     if (!param.key || param.key && !(param.key + '')) {
-      console.warn(`缓存失败，key不正确：${param.key}`);
+      console.warn(`${moduleName}-缓存失败，key不正确：${param.key}`);
       return;
     }
 
@@ -42,60 +44,72 @@ export default {
 
     // 参数检查-数据格式(不进行缓存的条件：数据为空或者转换异常)
     if (!param.data || param.data && !(param.data + '')) {
-      console.warn(`缓存失败，data不正确：${param.key}`);
+      console.warn(`${moduleName}-缓存失败，data不正确：${param.key}`);
       return;
     }
     try {
       JSON.stringify(param.data);
     } catch (err) {
-      console.warn(`缓存失败，data参数错误：${param.data}`);
+      console.warn(`${moduleName}-缓存失败，data参数错误：${param.data}`);
       return;
     }
 
     // 生成缓存value
-    let value = JSON.stringify({
+    let value = {
       now: Date.now(),
       expires: param.expires || 0,
       data: param.data,
-    });
+    };
 
     // 存入localStorage
-    window.localStorage.setItem(param.key, value);
+    window.localStorage.setItem(param.key, JSON.stringify(value));
 
-    // 日志
-    console.log(`调用localCache模块缓存完成：${param.key}=${value}`);
+    // 设置缓存成功日志
+    console.log(`%c${moduleName}-设置缓存：${param.key}`, `color: green;`);
+    // console.log(value);
   },
+
   /**
    * 取缓存数据
    * 调用方法：
-   * localCache.getItem(key)
+     localCache.getItem(key)
    */
   getItem(key) {
-    if (!key) {
-      throw new Error('key不对');
-      return;
-    }
+    // 取缓存
     let value = window.localStorage.getItem(key);
 
     // 参数不对
-    if (!value) return null;
+    if (!value) {
+      console.warn(`${moduleName}-找不到key：${key}`);
+      return null;
+    }
 
     // 数据转换异常
     try {
       value = JSON.parse(value);
     } catch (err) {
+      console.warn(`${moduleName}-缓存字符串转json异常：${key}`);
       return null;
     }
 
     // 不是本模块存的
-    if (!value.expires) return null;
+    if (!value.expires) {
+      console.warn(`${moduleName}-这条缓存不是本模块存储的：${key}`);
+      return null;
+    }
 
-    // 过期
+    // 缓存已过期
     if (Date.now() > new Date(value.now).valueOf() + value.expires || param.get('noCache') !== undefined) {
+      console.warn(`${moduleName}-缓存已过期：${key}`);
       window.localStorage.removeItem(key);
       return null;
     }
 
+    // 取缓存成功日志
+    console.log(`%c${moduleName}-取缓存：${key}`, `color: green;`);
+    // console.log(value.data);
+
+    // 返回数据
     return value.data;
   }
 }
