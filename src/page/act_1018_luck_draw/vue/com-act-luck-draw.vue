1c4021dc998ef0bfa24768e474f5dbad71f79889
donyyang <592645778@qq.com>
2017-10-19 15:49:46 +0800

    luck draw

--- src/page/act_1018_luck_draw/vue/com-act-luck-draw.vue
+++ src/page/act_1018_luck_draw/vue/com-act-luck-draw.vue
@@ -4,7 +4,7 @@
 <template>
 	<div class = "luckDraw">
     <div class = "luckTitle">会员订单支付金额累计每满500元可抽奖一次</div>
-    <div class = "luckNumNav">您有{{ luckNum }}次抽奖机会</div>
+    <div v-if = "isLogin && luckNum" class = "luckNumNav">您有{{ luckNum }}次抽奖机会</div>
     <div class = "luckCont" id="lottery">
       <table border="0" cellpadding="0" cellspacing="0">
         <tr>
@@ -25,33 +25,37 @@
       </table>
     </div>
     <!-- <div class = "mask"></div> -->
-    <div v-if = "isMoney" class = "moneyCont">
-      <img src="http://note.youdao.com/yws/res/1629/WEBRESOURCEf68d2764a1c666f4414be7e3b4bfb29f">
+    <div @click = "handleMoney" v-if = "isMoney" class = "moneyCont">
+      <div class = "moneyWrapper"></div>
+      <!-- <img src="http://note.youdao.com/yws/res/1629/WEBRESOURCEf68d2764a1c666f4414be7e3b4bfb29f"> -->
+      <img src="http://pic.davdian.com/free/ydd1.png">
       <div class = "moneyNum">
-        <p>恭喜你抽中免单</p>
-        <p>为你免单<span>xx</span>元</p>
+        <p>恭喜你抽中</p>
+        <p><span>{{ bounsNumMoney }}元</span>现金大奖</p>
       </div>
       <div class = "linkCont">
         <!-- 去购物 -->
-        <a class = "goShopLink" href=""></a>
+        <a @click = "handleGoShlping" class = "goShopLink" href="javascript:void(0)"></a>
         <!-- 查看账户  -->
         <a class = "lookLink" href=""></a>
       </div>
     </div>
-    <div v-if = "isBouns" class = "bounsCont">
-      <img src="http://note.youdao.com/yws/res/1627/WEBRESOURCE967a424bdc653fc1cc494478785b45ba">
-      <div class = "bounsInfo">
+    <div v-if = "isBouns" class = "bounsCont" @click = "handleBouns">
+      <!-- <img src="http://note.youdao.com/yws/res/1627/WEBRESOURCE967a424bdc653fc1cc494478785b45ba"> -->
+      <div class = "bounsWrapper"></div>
+      <img src="http://pic.davdian.com/free/ydd2.png">
+      <div class = "bounsInfo" v-if = "bounsInfos">
         <div class = "bounsNav">
-          <div class = "bouns_Nums"><span class = "bouns_m">¥</span><span class = "bouns_N">10</span><span class = "bouns_nm">元</span></div>
-          <div class = "bouns_limit">满10元可用</div>
+          <div class = "bouns_Nums"><span class = "bouns_m">¥</span><span class = "bouns_N">{{ bounsInfos.bonusMoney }}</span><span class = "bouns_nm">元</span></div>
+          <div class = "bouns_limit">满{{ bounsInfos.minConsumePrice }}元可用</div>
         </div>
         <div class = "bounsNav2">
           <div class = "bouns_b">仅购买</div>
-          <div class = "bouns_l">袋鼠妈妈品牌商品可用</div>
-          <div class = "bouns_d">2017.09.07 - 2017.09.07</div>
+          <div class = "bouns_l">{{ bounsInfos.bonusName }}</div>
+          <div class = "bouns_d">{{ bounsInfos.useBeginTime }} - {{ bounsInfos.useEndTime }}</div>
         </div>
       </div>
-      <div class = "bounsLink"></div>
+      <div @click = "handleGoShlping" class = "bounsLink"></div>
     </div>
 	</div>
 </template>
@@ -83,7 +87,11 @@
         isLogin: false,
         visitorStatus: 1,
         isMoney: false,
-        isBouns: true,
+        isBouns: false,
+        lotteryId: 0,
+
+        bounsNumMoney: 0,
+        bounsInfos: null,
       }
     },
     props: [],
@@ -102,6 +110,21 @@
       },
     },
     methods: {
+      handleMoney(e) {
+        if(e.target.className != "moneyWrapper") {
+          this.isMoney = false;
+        }
+      },
+      handleBouns(e) {
+        if (e.target.className != "bounsWrapper") {
+          this.isBouns = false;
+        }
+        // console.log(e.target)
+      },
+      handleGoShlping() {
+        this.isMoney = false;
+        this.isBouns = false;
+      },
       getData() {
         let that = this;
         $.ajax({
@@ -199,35 +222,79 @@
             that.prize=-1;
             that.times=0;
             that.click=false;
-
-            $.ajax({
-              url: "/api/mg/sale/returnbonus/lotteryBonus",
-              type: "POST",
-              dataType: "JSON",
-              data: layout.strSign('lottory_luck',{}),
-              success(res) {
-                console.log("res",res);
-                if (!res.code) {
-                  that.luckNum--;
-
-
-                } else {
-                  // popup.info()
-                }
-              },
-              error(error) {
-                console.log('error',error);
-              },
-            })
-
-
             console.log(1111111,that.index);
+            if (that.prize == 0 || that.prize == 2) {
+              that.isMoney = true;
+              if (that.prize == 0) {
+                that.bounsNumMoney = 10.18;
+              } else {
+                that.bounsNumMoney = 1018;
+              }
+            } else {
+              that.isBouns = true;
+              // that.bounsInfos = res.data.bonusInfo;
+            }
           }else{
             if (that.times<that.cycle) {
               that.speed -= 10;
             }else if(that.times==that.cycle) {
               // var index = Math.random()*(that.count)|0;
-              that.prize = 4;  
+              $.ajax({
+                url: "/api/mg/sale/returnbonus/lotteryBonus",
+                type: "POST",
+                async: false,
+                dataType: "JSON",
+                data: layout.strSign('lottory_luck',{}),
+                success(res) {
+                  console.log("res",res);
+                  if (!res.code) {
+                    that.luckNum--;
+                    if (res.data.lotteryResult == 'success') {
+                      // 最后两个是现金，其它按次序排的
+                      switch(res.data.bonusInfo.bonusTypeId) {
+                        case 2594: 
+                          that.prize = 1;
+                          break;
+                        case 2593:
+                          that.prize = 3;
+                          break;
+                        case 2592:
+                          that.prize = 4;
+                          break;
+                        case 2588:
+                          that.prize = 5;
+                          break;
+                        case 2585:
+                          that.prize = 5;
+                          break;
+                        case 2582:
+                          that.prize = 6;
+                          break;
+                        case 2597:
+                          that.prize = 0;
+                          break;
+                        case 2596:
+                          that.prize = 2;
+                          break;
+                      }
+                      if (res.data.bonusInfo) {
+                        res.data.bonusInfo.minConsumePrice = parseFloat(Number(res.data.bonusInfo.minConsumePrice));
+                        res.data.bonusInfo.bonusMoney = parseFloat(Number(res.data.bonusInfo.bonusMoney));
+
+                        that.bounsInfos = res.data.bonusInfo;
+                      }
+                    } else {
+                      // 不为success,为fail
+                      popup.info(res.data.msg);
+                    }
+                  } else {
+                    // popup.info()
+                  }
+                },
+                error(error) {
+                  console.log('error',error);
+                },
+              }) 
             }else{
               if (that.times > that.cycle+10 && ((that.prize==0 && that.index==7) || that.prize==that.index+1)) {
                 that.speed += 110;
